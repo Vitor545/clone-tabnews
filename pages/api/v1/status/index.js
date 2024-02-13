6e911bf43d57e43a1aa8edcdb923f3be1ec4477a
Vitor Souza da Silva <39885926+Vitor545@users.noreply.github.com>
2024-02-13 15:50:58 +0000

    adiciona `database.js` no projeto

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const reposta = await database.query("select 1 as sum");
+  console.log(reposta.rows);
   response.status(200).send("Olá api");
 }
 
